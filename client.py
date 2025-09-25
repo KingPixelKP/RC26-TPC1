02270a77bdf6ebfcb447aa41b2cb1ec66a0c15ec
Gabriel-Matias <gabrielmf1977@gmail.com>
2025-09-25 11:36:12 +0100

    Added constants to client

---
+++ client.py
@@ -0,0 +1,11 @@
+PROMPT : str = "client>"
+
+GET_CMD : str = "get"
+DIR_CMD : str = "dir"
+END_CMD : str = "end"
+
+GET_ERR1 : str = "Invalid number of arguments!!!"
+GET_ERR2 : str = "A file with the indicated name already exists on the client!!!"
+GET_ERR3 : str = "The indicated file does not exist on the server!!!"
+
+
