5eec7877c320b4d3eaa5c5dfdf179a3576c21e1c
Gabriel-Matias <gabrielmf1977@gmail.com>
2025-09-25 11:56:31 +0100

    Added constants to client

--- client.py
+++ client.py
@@ -1,3 +1,7 @@
+import pickle
+from socket import *
+import sys
+
 PROMPT : str = "client>"
 
 GET_CMD : str = "get"
@@ -8,4 +12,12 @@ GET_ERR1 : str = "Invalid number of arguments!!!"
 GET_ERR2 : str = "A file with the indicated name already exists on the client!!!"
 GET_ERR3 : str = "The indicated file does not exist on the server!!!"
 
+def main():
+
+    host_ip = sys.argv[1]
+    host_port = sys.argv[2]
+
+    client_socket = socket(AF_INET,SOCK_STREAM)
+
+    
 
