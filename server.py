c890b404cc6c908de2ace8f08d66a8faed0dfb3b
Dinis-Neves <dinis15neves@gmail.com>
2025-09-26 17:53:28 +0100

    not done yet, starting the pickling

--- server.py
+++ server.py
@@ -6,24 +6,41 @@ from socket import *
 import threading
 import time
 import os
+import pickle
+
+serverPort : int = 12000
+socketBuffer : int = 1024
+SIZE : int = 512
 
 
-# Start Server
-SERVER_RUNNING = "Server is running"
-SERVER_NOT_START = "Unable to start server"
+## Start Server
+SERVER_RUNNING : str = "Server is running"
+SERVER_NOT_START : str = "Unable to start server"
 
-# Recieve client
-WELCOME_CLIENT1 = "Welcome to "
-WELCOME_CLIENT2 = " file server"
+## Recieve client
+WELCOME_CLIENT1 : str = "Welcome to "
+WELCOME_CLIENT2 : str = " file server"
 
-#ERR
-FILE_NOT_FOUND = "File not found"
-
-
+## Errors
+FILE_NOT_FOUND : str = "File not found"
 
 
-serverPort = 12000
-socketBuffer = 1024
+## OP Codes
+REQUET_FILE_OP : int = 1
+DATA_OP : int = 3
+ACKNOWLEDGE_OP : int = 4
+
+
+
+
+
+
+def send_acknowledge_block(block : int, socket : socket):
+    acknowledge = (ACKNOWLEDGE_OP, block)
+    ack = pickle.dumps(acknowledge)
+    socket.send(ack)
+
+
 
 
 # Function to handle client, called always with a new threaded
@@ -38,6 +55,36 @@ def handle_client(c):
     print("received from client: ", dataD)    
     c.send(data)
   c.close()
+
+
+# Function to do the dir command
+def dir_command():
+   dir_path = "."
+   dir_list = os.listdir(dir_path)
+      ## send to client
+    
+   for x in dir_list:
+      print(x)
+
+
+# Function to do the get command server side
+def get_command(fName):
+    try: 
+      f = open(fName, "rb")
+    except:
+       print(FILE_NOT_FOUND)
+    
+    data = f.read(SIZE)
+    while (data):
+       ## send to client
+       sdata : tuple = (DATA_OP, data)
+       ssdat = pickle.dumps(sdata)
+       socket.send(ssdat)
+
+
+## end ???
+'''client closes socket but  server keep going'''
+   
 
 
 
@@ -63,15 +110,6 @@ def main():
         print('Connected to:', addr[0], ':', addr[1])
         tid = threading.Thread(target=handle_client, args = (c,))
         
-        # KeyBoardInterrupt
-        try:
-            print(".", end=' ',flush=True)
-            time.sleep(1)
-        except KeyboardInterrupt:
-            print("Exiting!")
-            break
-        print("Ending ")
-
-
-
+        break
+    
 main()
